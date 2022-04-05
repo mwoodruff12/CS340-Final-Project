611859f7f9a6da2b8a638825224acc2595a8d7be
Travis Honaker <thonaker3@capital.edu>
2022-04-04 22:48:14 -0400

    Update finalProj.rb
    added the base classes for the code

--- finalProj.rb
+++ finalProj.rb
@@ -1,1 +1,24 @@
 # CS Final 
+class WORDGAME
+  def init()
+  end
+  def intro()
+    puts("This game is a secret word guessing game. Enjoy and good luck!")
+  end
+  def play(String word)
+  end
+  def guessCheck(String g)
+  end
+  def update(String word, String cH, char nG)
+  end
+  def random(String file)
+    count = 0
+    returnVal = ""
+  end
+end
+
+def main
+  test = WORDGAME.new()
+  test.intro
+end
+main()
