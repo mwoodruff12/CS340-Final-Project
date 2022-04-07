3f64407f54a93c5443ae87732c3772e5ce3757df
Travis Honaker <thonaker3@capital.edu>
2022-04-07 13:57:26 -0400

    Update codeSnippets.rb

--- codeSnippets.rb
+++ codeSnippets.rb
@@ -1,9 +1,15 @@
 def main()
-  words = Array.new()
-  file = File.foreach("dict.txt") { |line| words << line}
-  count = words.size()
-  randomVal = rand(0..count)
-  puts randomVal - 1
-  puts words[randomVal]
+  # words = Array.new()
+  # file = File.foreach("dict.txt") { |line| words << line}
+  # count = words.size()
+  # randomVal = rand(0..count)
+  # puts randomVal - 1
+  # puts words[randomVal]
+  x = 12
+  if x > 2
+    puts("hig")
+  else
+    puts("low")
+  end
 end
 main()
