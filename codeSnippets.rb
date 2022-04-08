fd72f3b2b4f5cebe02044c881d7e1b16e8fb272b
Travis Honaker <thonaker3@capital.edu>
2022-04-08 14:09:32 -0400

    Update codeSnippets.rb

--- codeSnippets.rb
+++ codeSnippets.rb
@@ -1,17 +1,12 @@
 def main()
-  # words = Array.new()
-  # file = File.foreach("dict.txt") { |line| words << line}
-  # count = words.size()
-  # randomVal = rand(0..count)
-  # puts randomVal - 1
-  # puts words[randomVal]
-  # x = 12
-  # if x > 2
-  #   puts("hig")
-  # else
-  #   puts("low")
-  # end
-  test = "hello"
-  puts(test.length)
+  temp = Integer(gets)
+  while (temp != 0)
+    choice = case
+    when temp > 32 && temp < 60 then puts("its nice outside")
+    when temp >= 60 then puts("its warm outside")
+    when temp < 32 then puts("its cold outside")
+    end
+    temp = Integer(gets)
+  end
 end
 main()
