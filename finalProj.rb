72b3e52bfd7d003ada0b63ab9acf024cb3c32d69
Travis Honaker <thonaker3@capital.edu>
2022-04-08 10:24:58 -0400

    Update finalProj.rb

--- finalProj.rb
+++ finalProj.rb
@@ -78,8 +78,8 @@ class WORDGAME
     while (@_hint.include?("-")) do
       # outputs the current hint, how many guesses are left, and their total guesses
       guessLeftString = String(guessLeft)
-      H2 = String(@_hint)
-      puts(hintDisplay + H2)
+      # H2 = String(@_hint)
+      # puts(hintDisplay + H2)
       puts(guessLeftDisplay + guessLeftString)
       puts(guessesDisplay + @_guesses)
       
