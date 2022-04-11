1cc0cc6bd2a4082aa0dcf32bb76aa34cb232abe7
Travis Honaker <thonaker3@capital.edu>
2022-04-10 23:21:47 -0400

    Update codeSnippets.rb

--- codeSnippets.rb
+++ codeSnippets.rb
@@ -24,6 +24,42 @@ def main()
   #   puts("its nice outside")
   # end
 
+  #set x to be an integer value
+  # Integer x = 23
+
+  # #set y to be a float or double value
+  # Float y = 45.0
+
+  # #set s to be a string
+  # String s = "Hello World"
+
+  # #set c to be a character
+  # Char c = 'c'
+
+  #creates an integer variable
+  int = 60
+
+  #creates a float variable
+  flt = 25.60
+
+  #creates a string variable
+  str = "hello world"
+
+  #creates 2 boolean variables
+  bool1 = true
+
+  bool2 = false
+
+
+  #creates a hash table (python dictionary)
+  hsh = {"1" => "hello", "2" => "world"}
+
+  #creates an array, can hold different types of values
+  arry = ["hello", 24, 'c', 67.0]
+
+  #symbols are shortened strings that can be used in hash tables as keys
+  sym = {:sy => "symbols"}
+
 
 
   
